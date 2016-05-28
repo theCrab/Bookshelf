b138cc6581a0466847eee9e2733687cde8f7e5e2
The Crab <nelson@safarista.com>
2016-05-28 21:04:52 +0100

    Add "/books" endpoint test

--- spec/web/features/list_books_spec.rb
+++ spec/web/features/list_books_spec.rb
@@ -1,11 +1,16 @@
 require 'features_helper'
 
 describe 'List books' do
+  it 'should be successful' do
+    visit '/books'
+    
+    assert_equal(page.status_code, 200, 'OK')
+  end
+
   it 'displays each book on the page' do
     visit '/books'
 
     within '#books' do
-      # assert page.has_content?('Martin Fowler')
       # assert page.has_content?('Kent Beck')
       assert page.has_css?('.book', count: 2), 'Expected to find 2 books'
     end
