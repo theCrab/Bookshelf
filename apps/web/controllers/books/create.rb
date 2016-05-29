d1a4ee1140ad73132d3e2abcf562280fd91c18ed
The Crab <nelson@safarista.com>
2016-05-29 23:05:40 +0100

    Refactor and add better errro handling

--- apps/web/controllers/books/create.rb
+++ apps/web/controllers/books/create.rb
@@ -3,11 +3,18 @@ module Web::Controllers::Books
     include Web::Action
 
     expose :book
+    params do
+      param :book do
+        param :title, presence: true
+        param :author, presence: true
+      end
+    end
 
     def call(params)
-      @book = BookRepository.create(Book.new(params[:book]))
-
-      redirect_to '/books'
+      if params.valid?
+        @book = BookRepository.create(Book.new(params[:book]))
+        redirect_to '/books'
+      end
     end
   end
 end
--- spec/web/controllers/books/create_spec.rb
+++ spec/web/controllers/books/create_spec.rb
@@ -3,23 +3,43 @@ require_relative '../../../../apps/web/controllers/books/create'
 
 describe Web::Controllers::Books::Create do
   let(:action) { Web::Controllers::Books::Create.new }
-  let(:params) { Hash[book: { title: 'The Concubines', author: 'Nicholai Gogol' }] }
+  # let(:params) { Hash[book: { title: 'The Concubines', author: 'Nicholai Gogol' }] }
 
-  before do
+  after do
     BookRepository.clear
   end
 
-  it 'creates a new book' do
-    action.call(params)
+  describe 'with valid params' do
+    let(:params) { Hash[book: { title: 'The Concubines', author: 'Nicholai Gogol' }] }
 
-    action.book.id.wont_be_nil
-    action.book.title.must_equal(params[:book]['title'])
+    it 'creates a new book' do
+      action.call(params)
+      action.book.id.wont_be_nil
+    end
+
+    it 'redirects the user to the books listing' do
+      response = action.call(params)
+
+      response[0].must_equal(302)
+      response[1]['Location'].must_equal('/books')
+    end
   end
 
-  it 'redirects the user to the books listing' do
-    response = action.call(params)
+  describe 'with invalid params' do
+    let(:params) { Hash[book: {}] }
 
-    response[0].must_equal(302)
-    response[1]['Location'].must_equal('/books')
+    it 're-renders the book#new view' do
+      response = action.call(params)
+      response[0].must_equal(200)
+    end
+
+    it 'sets errors attribute accordingly' do
+      action.call(params)
+
+      refute action.params.valid?
+
+      action.errors.for('book.title').wont_be_empty
+      action.errors.for('book.author').wont_be_empty
+    end
   end
 end
